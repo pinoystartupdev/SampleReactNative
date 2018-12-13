215a2deb2060de243a484096097323b8498d9b94
Christian Claro T. Feraer <ctferaer@tthi.local>
2018-12-13 15:20:43 +0800

    add section list basic

--- App.js
+++ App.js
@@ -7,29 +7,25 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, FlatList} from 'react-native';
+import {Platform, StyleSheet, Text, View, SectionList} from 'react-native';
 
 type Props = {};
 
-export default class App extends Component<> {
+export default class App extends Component {
     render() {
-            return (
-                <View style={styles.container}>
-                        <FlatList
-                            data={[
-                                    {key: 'Devin'},
-                                    {key: 'Jackson'},
-                                    {key: 'James'},
-                                    {key: 'Joel'},
-                                    {key: 'John'},
-                                    {key: 'Jillian'},
-                                    {key: 'Jimmy'},
-                                    {key: 'Julie'},
-                            ]}
-                            renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-                        />
-                </View>
-            );
+        return (
+            <View style={styles.container}>
+                <SectionList
+                    sections={[
+                        {title: 'D', data: ['Devin']},
+                        {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+                    ]}
+                    renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+                    renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+                    keyExtractor={(item, index) => index}
+                />
+            </View>
+        );
     }
 }
 
@@ -38,9 +34,19 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: 22
     },
+    sectionHeader: {
+        paddingTop: 2,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 2,
+        fontSize: 14,
+        fontWeight: 'bold',
+        backgroundColor: 'rgba(247,247,247,1.0)',
+    },
     item: {
         padding: 10,
         fontSize: 18,
         height: 44,
     },
 });
+
