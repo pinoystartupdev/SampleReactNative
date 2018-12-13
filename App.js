b7b2d11ad5242becef32aad604c50d789b82c187
Christian Claro T. Feraer <ctferaer@tthi.local>
2018-12-13 10:12:41 +0800

    use state to blink

--- App.js
+++ App.js
@@ -18,12 +18,26 @@ const instructions = Platform.select({
 
 type Props = {};
 
-class Greeting extends Component {
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isShowingText: true };
+
+    // Toggle the state every second
+    setInterval(() => (
+        this.setState(previousState => (
+            { isShowingText: !previousState.isShowingText }
+        ))
+    ), 1000);
+  }
+
   render() {
+    if (!this.state.isShowingText) {
+      return null;
+    }
+
     return (
-        <View style={{alignItems: 'center'}}>
-          <Text>Hello {this.props.name}!</Text>
-        </View>
+        <Text>{this.props.text}</Text>
     );
   }
 }
@@ -31,10 +45,11 @@ class Greeting extends Component {
 export default class App extends Component<Props> {
   render() {
     return (
-        <View style={{alignItems: 'center'}}>
-          <Greeting name='Rexxar' />
-          <Greeting name='Jaina' />
-          <Greeting name='Valeera' />
+        <View>
+          <Blink text='I love to blink' />
+          <Blink text='Yes blinking is so great' />
+          <Blink text='Why did they ever take this out of HTML' />
+          <Blink text='Look at me look at me look at me' />
         </View>
     );
   }
