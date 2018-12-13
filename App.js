3cd96a5d655506e6dafce8309f580063ec306423
Christian Claro T. Feraer <ctferaer@tthi.local>
2018-12-13 11:18:32 +0800

    add flexDirection and justifyContent

--- App.js
+++ App.js
@@ -21,8 +21,13 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
       return (
-          // Try setting `flexDirection` to `column`.
-          <View style={{flex: 1, flexDirection: 'row'}}>
+          // Try setting `justifyContent` to `center`.
+          // Try setting `flexDirection` to `row`.
+          <View style={{
+              flex: 1,
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+          }}>
               <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
               <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
               <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
