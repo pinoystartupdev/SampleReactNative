93bc1db9662b3a3ce392c95cfafa28c0d95d5190
Christian Claro T. Feraer <ctferaer@tthi.local>
2018-12-13 10:32:47 +0800

    show fixed size for views

--- App.js
+++ App.js
@@ -18,50 +18,14 @@ const instructions = Platform.select({
 
 type Props = {};
 
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isShowingText: true };
-
-    // Toggle the state every second
-    setInterval(() => (
-        this.setState(previousState => (
-            { isShowingText: !previousState.isShowingText }
-        ))
-    ), 1000);
-  }
-
-  render() {
-    if (!this.state.isShowingText) {
-      return null;
-    }
-
-    return (
-        <Text>{this.props.text}</Text>
-    );
-  }
-}
-
 export default class App extends Component<Props> {
   render() {
     return (
         <View>
-          <Text style={styles.red}>just red</Text>
-          <Text style={styles.bigblue}>just bigblue</Text>
-          <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
-          <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+          <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+          <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
         </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  bigblue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
-});
