b5cc3ef6fc72a75a2c9d8da73575706ddf35fed6
Christian Claro T. Feraer <ctferaer@tthi.local>
2018-12-13 11:15:57 +0800

    add flexDirection

--- App.js
+++ App.js
@@ -21,13 +21,11 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
       return (
-          // Try removing the `flex: 1` on the parent View.
-          // The parent will not have dimensions, so the children can't expand.
-          // What if you add `height: 300` instead of `flex: 1`?
-          <View style={{flex: 1}}>
-              <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-              <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-              <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+          // Try setting `flexDirection` to `column`.
+          <View style={{flex: 1, flexDirection: 'row'}}>
+              <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+              <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+              <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
           </View>
       );
   }
